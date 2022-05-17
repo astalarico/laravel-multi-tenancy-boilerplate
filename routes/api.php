5c7fbdbccef98ff22778302bee71108cbe7615cd
Anthony Talarico <astalarico@gmail.com>
2022-05-17 08:17:08 -0400

    testing api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Member;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/members/{member}/{name}', function (Request $request, $memberName, $name) {
+    // $memberModel = new Member()->get();
+    return $memberName . ' ' . $name;
 });
